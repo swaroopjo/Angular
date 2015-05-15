20e8138fcb306aca7eab8a03ec528c0a1e6d1762
Ellipsis <swaroop_jo@yahoo.com>
2015-05-15 08:22:05 -0500

    Add Login Feature with Http basic authentication
    Fix Cross domain problem with JsonP

--- markitstockEx/WebContent/app/scripts/app.js
+++ markitstockEx/WebContent/app/scripts/app.js
@@ -1,15 +1,25 @@
 
 var app = angular.module('markitstockEx', [
-  'ngRoute'
+  'ngRoute',
+  'ngCookies'
 ]);
 	
 	app
 	.config(['$routeProvider',
 	                    function($routeProvider) {
 		$routeProvider.when('/',{
-			templateUrl:'/markitstockEx/app/views/main.html',
-			controller:'mainController'
-		}).otherwise({
+			templateUrl:'/markitstockEx/app/views/home.html',
+			controller:'homeController'
+		})
+		.when('/lookup',{
+			templateUrl:'/markitstockEx/app/views/lookup.html',
+			controller:'lookupController'
+		})
+		.when('/login',{
+			templateUrl:'/markitstockEx/app/views/login.html',
+			controller:'loginController'
+		})
+		.otherwise({
 	        redirectTo: '/'
 	      });
 	                    }]);
@@ -19,4 +29,22 @@ var app = angular.module('markitstockEx', [
 
 	      //Remove the header used to identify ajax call  that would prevent CORS from working
 	      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	  }).run(function($rootScope,$location,$cookieStore,$http){
+		  console.log("Inside Run function...Checking Authorization");
+		  //Get currentUserObject from cookie if present otherwise {}
+		  $rootScope.globals = $cookieStore.get('globals') || {};
+		 //Check if the globals are present in the cookie
+		  //If user is present, then set the AUthorization header to Basic username:password (encrypted)
+		  // The global should have an object of type currentUser:{userName:"",authData:""}
+		  if($rootScope.globals.currentUser){
+			  $http.defaults.headers.common['Authorization'] = 'Basic '+ $rootScope.globals.currentUser.authData;
+			  console.log("Setting http headers");
+		  }
+		  //For every Page change check if the current user exists.
+		  $rootScope.$on('$routeChangeStart',function(event,next,current){
+			  if($location.path() !== '/login' && !$rootScope.globals.currentUser){
+				  $location.path('/login');
+				  console.log("Checking if user exists...");
+			  }
+		  });
 	  });
